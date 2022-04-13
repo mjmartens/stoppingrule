e9d01db7a4beed83201b826a3dec827b6de12aa5
mjmartens <mmartens4681@gmail.com>
2022-04-12 19:46:12 -0500

    Fix help file

---
+++ R/stoppingrule.R
@@ -0,0 +1,14 @@
+#' Create and Evaluate Stopping Rules
+#'
+#' Package provides functions for creating, displaying, and evaluating stopping rules for safety monitoring in clinical studies.
+#'
+#'
+#' @docType package
+#'
+#' @author Michael J. Martens \email{mmartens@mcw.edu}
+#'
+#' @name stoppingrule
+#'
+#' @import stats
+#' @import utils
+NULL
