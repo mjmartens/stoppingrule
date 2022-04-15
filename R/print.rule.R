fd679b6f541656fa8509042fb0657b34d8252e85
mjmartens <mmartens4681@gmail.com>
2022-04-14 20:54:25 -0500

    Tweak documentation

--- R/print.rule.R
+++ R/print.rule.R
@@ -1,7 +1,7 @@
 #' @title Print Stopping Rule
 #' @description Display a stopping rule in tabular form
 #'
-#' @param x A rule object
+#' @param x A rule object, containing a matrix describing a stopping rule
 #' @param ... Other options to be passed to generic \code{plot} function
 #'
 #' @return A matrix with two columns: the sample sizes at which sequential testing is performed, and their corresponding rejection boundaries
