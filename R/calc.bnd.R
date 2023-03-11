76ac0c91274c2453daec747372b7d22a6947949e
mjmartens <mmartens4681@gmail.com>
2023-03-10 23:38:23 -0600

    Add calculation of stopping rules based on Chen and Chaloner method

--- R/calc.bnd.R
+++ R/calc.bnd.R
@@ -5,7 +5,7 @@
 #' @param p0 The toxicity rate under the null hypothesis
 #' @param cval Critical value for stopping rule method
 #' @param type The method used for constructing the stopping rule
-#' @param param Extra parameter(s) needed for certain stopping rule methods. For binomial Wang-Tsiatis tests, this is the Delta parameter. For Bayesian beta-binomial model, this is the pair of hyperparameters for the beta prior on the toxicity rate. For modified SPRT, this is the targeted alternative toxicity rate p1.
+#' @param param Extra parameter(s) needed for certain stopping rule methods. For binomial Wang-Tsiatis tests, this is the Delta parameter. For Bayesian beta-binomial model, this is the pair of hyperparameters for the beta prior on the toxicity rate. For Chen and Chaloner's method, this is the pair of hyperparameters for the beta prior on the toxicity rate, the targeted alternative toxicity rate p1, and the threshold for the posterior probability that p > p1. For modified SPRT, this is the targeted alternative toxicity rate p1.
 #'
 #' @return A vector of stopping boundaries at the sample sizes 1, 2, ..., n
 calc.bnd = function(n,p0,cval,type,param) {
@@ -25,6 +25,21 @@ calc.bnd = function(n,p0,cval,type,param) {
       postprob[j,1:(j+1)] = pbeta(p0,a+0:j,b+j:0,lower.tail=FALSE)
       if(max(postprob[j,]) > cval) {
         bs[j] = which(postprob[j,] > cval)[1]-1
+      }
+      else {bs[j] = j+1}
+    }
+  }
+  else if(type=="CC") {
+    a = param[1]
+    b = param[2]
+    p1 = param[3]
+    nu = param[4]
+    postprob0 = postprob1 = matrix(0,nrow=n,ncol=n+1)
+    for(j in 1:n) {
+      postprob0[j,1:(j+1)] = pbeta(p0,a+0:j,b+j:0,lower.tail=FALSE)
+      postprob1[j,1:(j+1)] = pbeta(p1,a+0:j,b+j:0,lower.tail=FALSE)
+      if(max(postprob0[j,]) > cval & max(postprob1[j,]) > nu) {
+        bs[j] = which(postprob0[j,] > cval & postprob1[j,] > nu)[1]-1
       }
       else {bs[j] = j+1}
     }
--- R/calc.rule.R
+++ R/calc.rule.R
@@ -3,20 +3,22 @@
 #'
 #' @param ns A vector of sample sizes at which sequential testing is performed
 #' @param p0 The toxicity rate under the null hypothesis
-#' @param type The method used for constructing the stopping rule. Choices include a Pocock test ("Pocock"), a Wang-Tsiatis test ("WT"), a Bayesian beta-binomial model ("BB"), a truncated SPRT ("SPRT"), and a maximized SPRT ("MaxSPRT").
-#' @param param Extra parameter(s) needed for certain stopping rule methods. For binomial Wang-Tsiatis tests, this is the Delta parameter. For Bayesian beta-binomial model, this is the pair of hyperparameters for the beta prior on the toxicity rate. For modified SPRT, this is the targeted alternative toxicity rate p1.
+#' @param type The method used for constructing the stopping rule. Choices include a Pocock test ("Pocock"), a Wang-Tsiatis test ("WT"), the Bayesian beta-binomial method ("BB") proposed by Geller et al. 2003, the Bayesian beta-binomial method ("CC") proposed by Chen and Chaloner 2006, a truncated SPRT ("SPRT"), and a maximized SPRT ("MaxSPRT").
+#' @param param Extra parameter(s) needed for certain stopping rule methods. For binomial Wang-Tsiatis tests, this is the Delta parameter. For Bayesian beta-binomial model, this is the pair of hyperparameters for the beta prior on the toxicity rate. For Chen and Chaloner's method, this is the pair of hyperparameters for the beta prior on the toxicity rate, the targeted alternative toxicity rate p1, and the threshold for the posterior probability that p > p1. For modified SPRT, this is the targeted alternative toxicity rate p1.
 #' @param alpha The desired type I error / false positive rate for the stopping rule
 #' @param iter The number of iterations used to search for the boundary
 #'
 #' @return A matrix with two columns: the sample sizes \code{ns} and their corresponding rejection boundaries
 #' @export
 #'
-#' @references Goldman, A.I. (1987). Issues in designing sequential stopping rules for monitoring side effects in clinical trials. \emph{Controlled clinical trials}  \strong{8(4)}, 327-37.
+#' @references Chen, C. and Chaloner, K. (2006). A Bayesian stopping rule for a single arm study: With a case study of stem cell transplantation. \emph{Statistics in medicine} \strong{25(17)}, 2956-66.
 #' @references Geller, N.L., Follman, D., Leifer, E.S. and Carter, S.L. (2003). Design of early trials in stem cell transplantation: a hybrid frequentist-Bayesian approach. \emph{Advances in Clinical Trial Biostatistics}.
+#' @references Goldman, A.I. (1987). Issues in designing sequential stopping rules for monitoring side effects in clinical trials. \emph{Controlled clinical trials}  \strong{8(4)}, 327-37.
 #' @references Ivanova, A., Qaqish, B.F. and Schell, M.J. (2005). Continuous toxicity monitoring in phase II trials in oncology. \emph{Biometrics} \strong{61(2)}, 540-545.
 #' @references Kulldorff, M., Davis, R.L., Kolczak, M., Lewis, E., Lieu, T. and Platt, R. (2011). A maximized sequential probability ratio test for drug and vaccine safety surveillance. \emph{Sequential analysis} \strong{30(1)}, 58-78.
 #' @references Pocock, S.J. (1977). Group sequential methods in the design and analysis of clinical trials. \emph{Biometrika} \strong{64(2)}, 191-199.
 #' @references Wang, S.K. and Tsiatis, A.A. (1987). Approximately optimal one-parameter boundaries for group sequential trials. \emph{Biometrics} \strong{193-199}.
+#'
 #' @examples
 #' # Binomial Pocock test in 50 patient cohort at 10% level, expected toxicity rate of 20%
 #' calc.rule(ns=1:50,p0=0.20,type="Pocock",alpha=0.10)
@@ -36,6 +38,9 @@ calc.rule = function(ns,p0,type,param=NULL,alpha,iter=50) {
   }
   else if(type=="BB") {
     const = findconst(ns,p0,"BB",alpha,0,1,iter,param)
+  }
+  else if(type=="CC") {
+    const = findconst(ns,p0,"CC",alpha,0,1,iter,param)
   }
   else if(type=="SPRT") {
     l = 0.5*qchisq(1-alpha,1)
